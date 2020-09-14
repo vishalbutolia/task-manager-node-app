5f8a3d41ff098c203d850e0540378d79f6b6361f
vishalbutolia <vishalbutolia@gmail.com>
2020-09-14 23:30:59 +0530

    feat. - added logout/logoutAll apis, added jwt tokenin incoming request

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,6 +11,7 @@ const authenticateUser = async (req, res, next) => {
       throw new Error("User not found");
     }
     
+    req.token = token;
     req.user = user;
     next();
   } catch (e) {
